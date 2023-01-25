187f3f15bc75b69c9c4e68318faf81850bb451ae
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2023-01-25 17:25:50 -0500

    a

--- slices/DetailMainText/index.js
+++ slices/DetailMainText/index.js
@@ -8,12 +8,16 @@ const DetailMainText = ({ slice }) => {
       {slice.variation !== "noReadMore" ? (
         <div className="col-start-3 relative leading-[4vw] md:leading-7 text-[3.3vw] md:text-[22px] text-center w-[94vw] md:w-[650px] lg:w-[800px] xl:w-[900px] mt-[2vw] mb-[2vw] md:mt-[40px] md:mb-[40px] lg:mt-[50px] lg:mb-[50px]">
           <div className="text-left">
-            <div className={`mt-0`}>{slice.primary.summary[0].text}</div>
-            {slice.primary.summary.slice(1).map((t, i) => (
-              <div key={i} className={`mt-5`}>
-                {t.text}
-              </div>
-            ))}
+            {slice.primary.summary[0] && (
+              <>
+                <div className={`mt-0`}>{slice.primary.summary[0].text}</div>
+                {slice.primary.summary.slice(1).map((t, i) => (
+                  <div key={i} className={`mt-5`}>
+                    {t.text}
+                  </div>
+                ))}
+              </>
+            )}
 
             <div className={`${isReadMore ? "hidden" : "block"}`}>
               {slice.primary.moreInfo.map((t, i) => (
