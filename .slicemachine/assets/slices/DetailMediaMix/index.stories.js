fa9e4aa564f467a4e31114528ec5f73fbaf3d0cf
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2023-02-06 12:21:14 -0500

    helo

--- .slicemachine/assets/slices/DetailMediaMix/index.stories.js
+++ .slicemachine/assets/slices/DetailMediaMix/index.stories.js
@@ -5,23 +5,26 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://slicemachine.dev"},"media1Autoplay":true,"media2":{"link_type":"Web","url":"http://twitter.com"},"media2Autoplay":true},"id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://prismic.io"},"media1Autoplay":false,"media2":{"link_type":"Web","url":"http://google.com"},"media2Autoplay":false},"id":"_Default"}} />
 _Default.storyName = 'Default'
 
-export const _3411 = () => <MyComponent slice={{"variation":"3411","name":"3/4 + 1/1","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://slicemachine.dev"},"media1Autoplay":true,"media2":{"link_type":"Web","url":"http://google.com"},"media2Autoplay":true},"id":"_3411"}} />
+export const _3411 = () => <MyComponent slice={{"variation":"3411","name":"3/4 + 1/1","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://slicemachine.dev"},"media1Autoplay":false,"media2":{"link_type":"Web","url":"https://slicemachine.dev"},"media2Autoplay":false},"id":"_3411"}} />
 _3411.storyName = '3/4 + 1/1'
 
-export const _4545 = () => <MyComponent slice={{"variation":"4545","name":"4/5 + 4/5","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://prismic.io"},"media1Autoplay":true,"media2":{"link_type":"Web","url":"https://slicemachine.dev"},"media2Autoplay":true},"id":"_4545"}} />
+export const _4545 = () => <MyComponent slice={{"variation":"4545","name":"4/5 + 4/5","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://slicemachine.dev"},"media1Autoplay":true,"media2":{"link_type":"Web","url":"http://google.com"},"media2Autoplay":true},"id":"_4545"}} />
 _4545.storyName = '4/5 + 4/5'
 
-export const _11Filter = () => <MyComponent slice={{"variation":"11Filter","name":"11Filter","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://prismic.io"},"media1Autoplay":true,"media2":{"link_type":"Web","url":"https://prismic.io"},"media2Autoplay":true},"id":"_11Filter"}} />
+export const _11Filter = () => <MyComponent slice={{"variation":"11Filter","name":"11Filter","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"http://twitter.com"},"media1Autoplay":false,"media2":{"link_type":"Web","url":"https://prismic.io"},"media2Autoplay":true},"id":"_11Filter"}} />
 _11Filter.storyName = '11Filter'
 
-export const _169916 = () => <MyComponent slice={{"variation":"169916","name":"16/9 + 9/16","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"http://google.com"},"media1Autoplay":false,"media2":{"link_type":"Web","url":"http://twitter.com"},"media2Autoplay":true},"id":"_169916"}} />
+export const _169916 = () => <MyComponent slice={{"variation":"169916","name":"16/9 + 9/16","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"http://google.com"},"media1Autoplay":true,"media2":{"link_type":"Web","url":"https://slicemachine.dev"},"media2Autoplay":false},"id":"_169916"}} />
 _169916.storyName = '16/9 + 9/16'
 
-export const _91616X9 = () => <MyComponent slice={{"variation":"91616X9","name":"9/16 + 16X9","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://prismic.io"},"media1Autoplay":false,"media2":{"link_type":"Web","url":"https://slicemachine.dev"},"media2Autoplay":false},"id":"_91616X9"}} />
+export const _91616X9 = () => <MyComponent slice={{"variation":"91616X9","name":"9/16 + 16X9","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"http://twitter.com"},"media1Autoplay":false,"media2":{"link_type":"Web","url":"http://google.com"},"media2Autoplay":false},"id":"_91616X9"}} />
 _91616X9.storyName = '9/16 + 16X9'
 
-export const _11169 = () => <MyComponent slice={{"variation":"11169","name":"1/1 + 16/9","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"https://slicemachine.dev"},"media1Autoplay":false,"media2":{"link_type":"Web","url":"https://prismic.io"},"media2Autoplay":false},"id":"_11169"}} />
+export const _11169 = () => <MyComponent slice={{"variation":"11169","name":"1/1 + 16/9","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"http://twitter.com"},"media1Autoplay":false,"media2":{"link_type":"Web","url":"https://slicemachine.dev"},"media2Autoplay":false},"id":"_11169"}} />
 _11169.storyName = '1/1 + 16/9'
+
+export const _16911 = () => <MyComponent slice={{"variation":"16911","name":"16/9 + 1/1","slice_type":"detail_media_mix","items":[],"primary":{"media1":{"link_type":"Web","url":"http://google.com"},"media1Autoplay":true,"media2":{"link_type":"Web","url":"https://prismic.io"},"media2Autoplay":false},"id":"_16911"}} />
+_16911.storyName = '16/9 + 1/1'
--- slices/DetailMediaMix/index.js
+++ slices/DetailMediaMix/index.js
@@ -378,6 +378,71 @@ const DetailMediaMix = ({ slice }) => {
             </div>
           )}
         </>
+      ) : slice.variation === "16911" ? (
+        <>
+          {slice.primary.media1.link_type === "Web" ? (
+            <div
+              className={`col-start-3 md:col-start-1 w-[94vw] md:w-[60.4vw] relative rounded md:place-self-start md:left-[2vw] aspect-[16/9]`}
+            >
+              <iframe
+                className="w-[100%] h-[100%]"
+                src={`${slice.primary.media1.url.slice(
+                  6
+                )}?quality=1080p&loop=1${
+                  slice.primary.media1Autoplay
+                    ? "&background=1&autoplay=1&muted=1&controls=0"
+                    : ""
+                }`}
+                allowFullScreen
+                autoPlay="1"
+              ></iframe>
+            </div>
+          ) : (
+            <div
+              className={`col-start-3 md:col-start-1 w-[94vw]  md:w-[60.4vw]  relative rounded md:place-self-start md:left-[2vw]  aspect-[16/9]`}
+            >
+              <Image
+                src={slice.primary.media1.url}
+                //   alt={thumbnails[0]}
+                priority={true}
+                loading="eager"
+                layout="fill"
+                objectFit="cover"
+              ></Image>
+            </div>
+          )}
+          {slice.primary.media2.link_type === "Web" ? (
+            <div
+              className={`col-start-3 md:col-start-5 w-[94vw]   md:w-[34vw] relative rounded md:place-self-end md:right-[2vw] aspect-square`}
+            >
+              <iframe
+                className="w-[100%] h-[100%]"
+                src={`${slice.primary.media2.url.slice(
+                  6
+                )}?quality=1080p&loop=1${
+                  slice.primary.media2Autoplay
+                    ? "&background=1&autoplay=1&muted=1&controls=0"
+                    : ""
+                }`}
+                allowFullScreen
+                autoPlay="1"
+              ></iframe>
+            </div>
+          ) : (
+            <div
+              className={`col-start-3 md:col-start-5 w-[94vw]  md:w-[34vw] relative rounded md:place-self-end md:right-[2vw]   aspect-square`}
+            >
+              <Image
+                src={slice.primary.media2.url}
+                //   alt={thumbnails[0]}
+                priority={true}
+                loading="eager"
+                layout="fill"
+                objectFit="cover"
+              ></Image>
+            </div>
+          )}
+        </>
       ) : (
         <>
           {slice.primary.media1.link_type === "Web" ? (
