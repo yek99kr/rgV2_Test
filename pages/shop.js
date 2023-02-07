e567ad43b3097d0f0327515fd0cf3d12ea386f43
Ye eun kim <rachelyeeunkim@Yes-MacBook-Pro-2.local>
2023-02-07 11:20:39 -0500

    align

--- pages/shop.js
+++ pages/shop.js
@@ -21,7 +21,7 @@ const store = ({ router }) => {
         className="fixed overflow-hidden w-[100vw] h-[100vw]"
       >
         <div className="md:w-[90vw] w-[100vw] h-[100vh] text-[4vw] 4xs:text-[4vw] sm:text-[22px] md:text-[22px] lg:text-[24px] xl:text-[25px]">
-          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[50%] font-bold ">
+          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[60%] font-bold ">
             <span className="wiggle1 text-pink-500">c</span>
             <span className="wiggle2 text-sky-400">o</span>
             <span className="wiggle3 text-yellow-500">m</span>
